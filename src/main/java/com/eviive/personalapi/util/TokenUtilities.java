2baafcac5fd54c1ea6a44add8fa3872487f7dfaf
Eviive <albert.vaillon21@gmail.com>
2023-07-01 14:26:06 +0200

    feat: cleanup of the TokenUtilities class and properties

--- src/main/java/com/eviive/personalapi/util/TokenUtilities.java
+++ src/main/java/com/eviive/personalapi/util/TokenUtilities.java
@@ -4,7 +4,6 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
-import jakarta.annotation.PostConstruct;
 import jakarta.servlet.http.Cookie;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -16,15 +15,12 @@ import java.util.List;
 @Component
 public final class TokenUtilities {
 
-    private Algorithm algorithm;
-    @Value("${jwt-secret-key}")
-    private String secret;
-    @Value("${is-production}")
-    private boolean isProduction;
+    private final Algorithm algorithm;
+    private final boolean isProduction;
 
-    @PostConstruct
-    public void init() {
-        algorithm = Algorithm.HMAC256(secret);
+    public TokenUtilities(@Value("${jwt-secret-key}") String secret, @Value("${is-production}") boolean isProduction) {
+        this.algorithm = Algorithm.HMAC256(secret);
+        this.isProduction = isProduction;
     }
 
     public String generateAccessToken(String subject, String issuer, List<String> claims) {
