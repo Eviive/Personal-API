09d5d2af0fe64966e7f27f8af083c2b522fd3e36
Eviive <albert.vaillon21@gmail.com>
2023-07-15 22:51:24 +0200

    fix: sending back 201 when saving with an Image

--- src/main/java/com/eviive/personalapi/controller/SkillController.java
+++ src/main/java/com/eviive/personalapi/controller/SkillController.java
@@ -46,7 +46,11 @@ public class SkillController {
 
     @PostMapping(path = "with-image", consumes = MULTIPART_FORM_DATA_VALUE, produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<SkillDTO> saveWithImage(@RequestPart("skill") @Valid SkillDTO skillDTO, @RequestPart("file") MultipartFile file) {
-        return ResponseEntity.ok().body(skillService.save(skillDTO, file));
+        SkillDTO createdSkillDTO = skillService.save(skillDTO, file);
+
+        URI uri = URI.create(String.format("/skill/%s", createdSkillDTO.getId()));
+
+        return ResponseEntity.created(uri).body(createdSkillDTO);
     }
 
     @PostMapping(path = "save-all", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
