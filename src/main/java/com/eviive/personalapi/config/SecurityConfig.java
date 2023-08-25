25444bfe91a27a19363fab1a36fd47eaad16a193
Eviive <albert.vaillon21@gmail.com>
2023-08-26 00:38:04 +0200

    fix: deprecated HttpSecurity method signatures

--- src/main/java/com/eviive/personalapi/config/SecurityConfig.java
+++ src/main/java/com/eviive/personalapi/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -36,36 +37,38 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.cors()
-                   .and()
-                   .csrf().disable()
-                   .sessionManagement().sessionCreationPolicy(STATELESS)
-                   .and()
+        return http.csrf(AbstractHttpConfigurer::disable)
+
+                   .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+
+                   .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
+
                    .addFilterBefore(authorizationFilter, UsernamePasswordAuthenticationFilter.class)
-                   .authorizeHttpRequests()
-                       .requestMatchers(POST, "/user/login", "/user/logout", "/user/refresh").permitAll()
-                       .requestMatchers("/user/**").hasAuthority(ROLE_ADMIN.toString())
 
-                       .requestMatchers("/role/**").hasAuthority(ROLE_ADMIN.toString())
+                   .authorizeHttpRequests(auth ->
+                           auth.requestMatchers(POST, "/user/login", "/user/logout", "/user/refresh").permitAll()
+                               .requestMatchers("/user/**").hasAuthority(ROLE_ADMIN.toString())
 
-                       .requestMatchers(GET, "/project/**").permitAll()
-                       .requestMatchers("/project/**").hasAuthority(ROLE_USER.toString())
+                               .requestMatchers("/role/**").hasAuthority(ROLE_ADMIN.toString())
 
-                       .requestMatchers(GET, "/skill/**").permitAll()
-                       .requestMatchers("/skill/**").hasAuthority(ROLE_USER.toString())
+                               .requestMatchers(GET, "/project/**").permitAll()
+                               .requestMatchers("/project/**").hasAuthority(ROLE_USER.toString())
 
-                       .requestMatchers(GET, "/image/**").permitAll()
-                       .requestMatchers("/image/**").hasAuthority(ROLE_USER.toString())
+                               .requestMatchers(GET, "/skill/**").permitAll()
+                               .requestMatchers("/skill/**").hasAuthority(ROLE_USER.toString())
 
-                       .requestMatchers("/actuator/**").hasAuthority(ROLE_ADMIN.toString())
+                               .requestMatchers(GET, "/image/**").permitAll()
+                               .requestMatchers("/image/**").hasAuthority(ROLE_USER.toString())
 
-                       .anyRequest().denyAll() // deny-by-default policy
-                   .and()
+                               .requestMatchers("/actuator/**").hasAuthority(ROLE_ADMIN.toString())
+
+                               .anyRequest().denyAll() // deny-by-default policy
+                   )
+
                    .build();
     }
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
+    private CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(allowedOrigins);
         configuration.addAllowedMethod("*");
